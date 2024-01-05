141ac6d25dbe037140392748e1edaff51f9bf653
amimanye <amimanyeaglago@gmail.com>
2024-01-05 19:21:10 +0000

    working on the script

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,23 @@
 const search_country = document.getElementById('search_country')
 const search_city = document.getElementById('search cities');
 
+/* getting the weather conidions */
+// const apiurl = 'https://api.openweathermap.org/data/2.5/weather?appid=d6e130c594758759a8a7966d8b2dbff2';
+/* retuns the longitude and latitude */
+
+const countries = 'https://countriesnow.space/api/v0.1/countries/positions';
+
 var latitude;
 var longitude;
 var country = null;
 var city = null;
-
-var country = search_country.value;
-var city = search_city.value;
-
-// function check_notnull (country, city) {
-//     if (country != null && city != null) {
-//         var country = search_country.value;
-//         var city = search_city.value;
-//     }
-// }
-
-
-/* getting the weather conidions */
-const apiurl = 'https://api.openweathermap.org/data/2.5/weather?appid=d6e130c594758759a8a7966d8b2dbff2';
-/* retuns the longitude and latitude */
-var location = `https://api.api-ninjas.com/v1/geocoding?city={city}&country={country}'`;
-
-/* calling longlat */
-// fetchlonglat = (city, country) {
-
-// }
+var country;
+var city;
 
 /* fetching the data from the the whether api */
 const fetchData = async () => {
     try {
-        const response = await fetch(apiurl);
+        const response = await fetch(countries);
         const data = await response.json();
         return data;       
     } catch (error) {
@@ -39,7 +26,7 @@ const fetchData = async () => {
     }
 }
 
-/* calling the fetchData function */
-fetchData().then((data) => {
-    console.log(data);
-});
+/* getting information from documentor.getpostman */
+
+data = fetchData();
+console.log(data)
