d512972529903287f9c93535b46f5c8793ed5fa4
amimanye <amimanyeaglago@gmail.com>
2024-01-06 19:45:45 +0000

    made slight changes to javascript file

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
-const search_country = document.getElementById('search_country')
-const search_city = document.getElementById('search cities');
 
-/* getting the weather conidions */
-// const apiurl = 'https://api.openweathermap.org/data/2.5/weather?appid=d6e130c594758759a8a7966d8b2dbff2';
-/* retuns the longitude and latitude */
-
-const countries = 'https://countriesnow.space/api/v0.1/countries/positions';
 
 var latitude;
 var longitude;
-var country = null;
-var city = null;
-var country;
-var city;
+var country = document.getElementById('country').value;
+var city = document.getElementById('city').value;;
+const apikey = 'd6e130c594758759a8a7966d8b2dbff2';
+const search_country = document.getElementById('search_country');
+const search_city = document.getElementById('search cities');
+const countries = 'https://countriesnow.space/api/v0.1/countries/positions';
+const countries_api = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={apikey}`;
+
+/* getting the weather conidions */
+// const apiurl = 'https://api.openweathermap.org/data/2.5/weather?appid=';
+/* retuns the longitude and latitude */
+
+
+
+
 
 /* fetching the data from the the whether api */
 
@@ -30,15 +34,20 @@ const fetchData = async () => {
 /* calling the fetchData function */
 
 fetchData().then((data) => {
-    console.log(data);
+    console.log(data)
 });
 
 
 /* function to create option element in select */
 
-const create_option = (data = fetchData()) => {
-    for (country of data.object.data);
-        console.log(country);
+const create_option = async () => {
+    try {
+        const data = fetchData();
+        for (country of data.data);
+            console.log(country);
+    } catch (error) {
+        console.error('Error:', error)
+    }
 }
 
 create_option();
