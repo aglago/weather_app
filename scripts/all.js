02b3723608374437a3e8790a5369239181123ebf
amimanye <amimanyeaglago@gmail.com>
2024-01-07 20:01:16 +0000

    function to keep all scripts

--- scripts/all.js
+++ scripts/all.js
@@ -1,18 +1,14 @@
-@
+import { create_countries } from './create_countries';
+import { fetchCities } from './fetch_cities';
+import { fetchCountries } from './fetch_countries';
 
 var latitude;
 var longitude;
 const apikey = 'd6e130c594758759a8a7966d8b2dbff2';
+const base_url = 'https://countriesnow.space/api/v0.1/countries/'
 
-const countries = 'https://countriesnow.space/api/v0.1/countries/positions';
-const cities = 'https://countriesnow.space/api/v0.1/countries/cities';
+const countries = `${base_url} + positions`;
+const cities = `${base_url} + cities`;
 const countries_api = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={apikey}`;
 const select_country = document.getElementById('countries');
 const select_city = document.getElementById('cities');
-
-
-/* getting the weather conidions */
-// const apiurl = 'https://api.openweathermap.org/data/2.5/weather?appid=';
-/* retuns the longitude and latitude */
-
-/* getting information from documentor.getpostman */
